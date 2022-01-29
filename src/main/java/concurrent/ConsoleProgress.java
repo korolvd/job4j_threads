66ea085d28e5a81877bfa743ed71750bde71be82
korolvd <ffirin@mail.ru>
2022-01-29 18:21:00 +0500

    3. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        String[] process = {"-", "\\", "|", "/"};
+        int posProcess = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\r load: " + process[posProcess++]);
+                posProcess = posProcess == 3 ? 0 : posProcess;
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
