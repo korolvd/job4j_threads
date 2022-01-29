eab1229e19ab9c4b70a7bc5f067c90ffe730bc95
korolvd <ffirin@mail.ru>
2022-01-29 17:14:06 +0500

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,17 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
