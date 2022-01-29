cf11a9b60f4c9ae43289a9201f6b2010b39877e5
korolvd <ffirin@mail.ru>
2022-01-29 19:46:37 +0500

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/thread/Wget.java
@@ -0,0 +1,51 @@
+package thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String outputFile = url.split("/")[url.split("/").length - 1];
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("out_" + outputFile)) {
+            byte[] dataBuffer = new byte[speed];
+            int bytesRead;
+            long currentTime = 0;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                currentTime = System.currentTimeMillis() - startTime;
+                if (currentTime < 1000) {
+                    try {
+                        Thread.sleep(1000 - currentTime);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                startTime = System.currentTimeMillis();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
