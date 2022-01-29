3826b1d4df6a5d9da7929ff9bb8e1d6be28bd8d3
korolvd <ffirin@mail.ru>
2022-01-29 20:37:39 +0500

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/Cache.java
@@ -0,0 +1,10 @@
+public class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
