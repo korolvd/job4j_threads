32dc0a665b324c3aaf6e5141520cdfee16208e0b
korolvd <ffirin@mail.ru>
2022-02-04 08:48:31 +0500

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,17 @@
+package ru.job4j.ref;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+}
