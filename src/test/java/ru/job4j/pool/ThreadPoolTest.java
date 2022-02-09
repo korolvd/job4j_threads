627459c9b2ccb7064d470bc32631cec0ddaebc08
korolvd <ffirin@mail.ru>
2022-02-09 18:35:21 +0500

    1. Реализовать ThreadPool [#1099 #262477]

---
+++ src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.pool;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ThreadPoolTest {
+
+    @Ignore
+    @Test
+    public void work() throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        List<Integer> buffer = new ArrayList<>();
+        Runnable task = () -> buffer.add(1);
+        for (int i = 0; i < 8; i++) {
+            threadPool.work(task);
+        }
+        Thread.sleep(3000);
+        threadPool.shutdown();
+        assertThat(buffer, is(List.of(1, 1, 1, 1, 1, 1, 1, 1)));
+    }
+}
